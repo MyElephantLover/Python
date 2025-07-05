0df2a0e2f630889554d578c17bb9775a1deeb4ae
MyElephantLover <jin.summer.1993@gmail.com>
2025-07-05 13:53:34 -0400

    edited

--- ReverseWordsinString.py
+++ ReverseWordsinString.py
@@ -2,5 +2,9 @@ class Solution:
     def reverseWords(self, strs: str) -> str:
         return " ".join(reversed(strs.split())) # what this line of code is doing, is it removes extra
     # whitespaces from the string strs, including leading and trailing spaces and multiple whitespaces
-    # between words, and return a cleaned-up version of the string with single spaces between words
+    # between words, and return a cleaned-up version of the string with single space between words
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
  
