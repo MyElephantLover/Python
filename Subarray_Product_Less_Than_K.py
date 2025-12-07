d4c10da2ef3b62d2b66c00c62785ce6f81670420
MyElephantLover <jin.summer.1993@gmail.com>
2025-12-07 16:47:05 -0500

    edited

--- Subarray_Product_Less_Than_K.py
+++ Subarray_Product_Less_Than_K.py
@@ -1,5 +1,6 @@
 # Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
 
+#=========Constraints==========#
 # 1 <= nums.length <= 3 * 104
 # 1 <= nums[i] <= 1000
 # 0 <= k <= 106
