fb8c91f9b19cf9f06b3d4d5dc712ab6a8c014c2a
MyElephantLover <jin.summer.1993@gmail.com>
2026-09-02 17:09:25 -0400

    add find_it

---
+++ Find_the_odd_int.py
@@ -0,0 +1,9 @@
+# Given an array of integers, find the one that appears an odd number of times.
+
+# There will always be only one integer that appears an odd number of times.
+
+def find_it(seq):
+    for num in seq:
+        if seq.count(num) % 2 == 1:
+            return num
+    return None
